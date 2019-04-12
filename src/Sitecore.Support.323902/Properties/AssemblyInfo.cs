0d7a371185c7b46ebad04a25eb30530ac116fb9e
Viacheslav Afanasyev <viaf@sitecore.net>
2019-04-12 08:29:01 +0300

    #Initial commit

---
+++ src/Sitecore.Support.323902/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.323902")]
+[assembly: AssemblyProduct("Sitecore.Support.323902")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.323902/RegisterPageEventProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Analytics;
+using Sitecore.Analytics.Outcome.Model;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.FXM.Pipelines.Tracking.RegisterPageEvent;
+using Sitecore.FXM.Utilities;
+
+namespace Sitecore.Support.FXM.Pipelines.Tracking.RegisterPageEvent
+{
+  public class RegisterPageEventProcessor : Sitecore.FXM.Pipelines.Tracking.RegisterPageEvent.RegisterPageEventProcessor
+  {
+    protected override void TriggerOutcome(RegisterPageEventArgs args)
+    {
+      if (Tracker.Current != null)
+      {
+        ID newID = ID.NewID;
+        ID interactionId = ID.Parse(Tracker.Current.Interaction.InteractionId);
+        ID contactId = ID.Parse(Tracker.Current.Contact.ContactId);
+        Item pageEventItem = args.PageEventItem;
+        ContactOutcome contactOutcome = new ContactOutcome(newID, pageEventItem.ID, contactId)
+        {
+          DateTime = DateTime.UtcNow.Date,
+          InteractionId = interactionId,
+          MonetaryValue = args.EventParameters.MonetaryValue
+        };
+        foreach (KeyValuePair<string, string> extra in args.EventParameters.Extras)
+        {
+          contactOutcome.CustomValues[extra.Key] = extra.Value;
+        }
+
+        OutcomeUtility.TriggerOutcome(contactOutcome);
+      }
+    }
+  }
+}
